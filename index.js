259427f25dff184ba2b1521ae7d059a89e006392
unlimited1974@aol.com <unlimited1974@aol.com>
2022-01-25 12:54:55 -0600

    jsupdate 03

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-function getCrypto(){
-  fetch("http://localhost:3000/Crypto")
-  .then(res => res.json())
-  .then(cryptoArray => buildCrypto(cryptoArray))
-}
 getCrypto()
 
-  const crypto = document.querySelector("#crypto")   
-function buildCrypto(cryptoArray){
-  
-  cryptoArray.forEach(cryptoObj => {
-  const cryptoLi = document.createElement("li")
-  cryptoLi.textContent = cryptoObj.crypto
-  const cryptoImg = document.createElement("img")
-  console.log(cryptoImg)
-  cryptoImg.src = cryptoObj.image
+function getCrypto() {
+  fetch("http://localhost:3000/Crypto")
+  .then(res => res.json())
+  .then(cryptoArray => console.log(cryptoArray))
+}
 
-  });
+
+ const crypto = document.querySelector("#crypto")
+
+
+function buildCrypto(cryptoArray) {
+    cryptoObj.message.forEach(cryptoMessage => {
+        const cryptoObj = document.createElement('img')
+        cryptoObj.src = cryptoMessage
     
+    })  
+//   cryptoArray.forEach(cryptoObj => {
+
+//   const cryptoLi = document.createElement("li")
+//   cryptoLi.textContent = cryptoObj.crypto
+
+// //   const cryptoImg = document.createElement("img")
+// //   cryptoImg.src = cryptoObj.image
+  
+
+//   });
+    
+// }
 }
