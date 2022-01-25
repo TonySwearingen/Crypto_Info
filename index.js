eb35a781d9247b8b866917d70602c2dd3487ae89
Themanual22 <djamosry22@gmail.com>
2022-01-25 11:55:28 -0500

    js update 01

---
+++ index.js
@@ -0,0 +1,16 @@
+function getCrypto(){
+  fetch("http://localhost:3000/Crypto")
+  .then(res => res.json())
+  .then(cryptoArray => console.log(cryptoArray))
+}
+getCrypto()
+
+const 
+function buildCrypto(){
+  cryptoArray.array.forEach(cryptoObj => {
+  const cryptoObj = document.createElement()
+  cryptoImg.src = cryptoObj.image
+
+  });
+    console.log(hello)
+}
